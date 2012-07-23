3e5ea3bdc66c75ae8689832636ca06f938b34935
chris reynolds <chris@jazzsequence.com>
2012-07-23 13:19:56 -0600

    adds value to searchform submit button

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
 <div><input type="text" value="search this site" name="s" id="s" onfocus="if (this.value == 'search this site')
 {this.value = '';}" onblur="if (this.value == '') {this.value = 'search this site';}" />
-<input type="submit" id="searchsubmit" value="" />
+<input type="submit" id="searchsubmit" value="Go" />
 </div>
 </form>
